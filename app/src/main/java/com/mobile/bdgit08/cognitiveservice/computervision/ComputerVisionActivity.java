d1e211614265ba220c800eb02948955bc289b8a8
bdgit08 <dior.tantri@gmail.com>
2019-05-02 11:24:06 +0700

    init
    first init project clone

---
+++ app/src/main/java/com/mobile/bdgit08/cognitiveservice/computervision/ComputerVisionActivity.java
@@ -0,0 +1,14 @@
+package com.mobile.bdgit08.cognitiveservice.computervision;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComputerVisionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
